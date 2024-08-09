32d26b0a115811acbe7e7aaa4a0ff8ef194379a2
Raphael Meyer <raphael.akira.meyer@gmail.com>
2024-08-09 22:52:46 +0200

    initial ui draft

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,20 @@
 module Main where
 
-import qualified Brick
-import qualified Brick.Widgets.Border as WB
-import qualified Brick.Widgets.Core as WC
+import qualified Brick.AttrMap as Attr
+import qualified Brick.Main as Brick
+import qualified Brick.Types as Types
+import qualified Brick.Widgets.Border as Border
+import qualified Brick.Widgets.Core as Core
 import qualified Control.Monad as M (void)
+import qualified Data.Map.Strict as Map
 import qualified Graphics.Vty
 
-data ConnectionState = ConnectionState {sHosts :: [String], sPorts :: [Int]} deriving (Eq, Show)
+data ConnectionState = ConnectionState
+  { sHosts :: [String],
+    sPorts :: [Int],
+    sConnections :: Map.Map String Int
+  }
+  deriving (Eq, Show)
 
 type Name = ()
 
@@ -15,7 +23,12 @@ main = do
   M.void $ Brick.defaultMain app initialState
 
 initialState :: ConnectionState
-initialState = ConnectionState {sHosts = ["foo", "bar"], sPorts = [1234, 4456]}
+initialState =
+  ConnectionState
+    { sHosts = ["foo", "bar", "abcdefghijklmnopqrstu"],
+      sPorts = [1234, 4456, 4000, 4001, 4002],
+      sConnections = Map.empty
+    }
 
 app :: Brick.App ConnectionState e Name
 app =
@@ -27,14 +40,16 @@ app =
       Brick.appAttrMap = const attributes
     }
 
-drawUI :: ConnectionState -> [Brick.Widget Name]
-drawUI s =
-  [ WC.vBox
-      [ WC.hBox . map Brick.str . sHosts $ s,
-        WB.hBorder,
-        WC.hBox . map (Brick.str . show) . sPorts $ s
-      ]
-  ]
+drawUI :: ConnectionState -> [Types.Widget Name]
+drawUI s = [Core.hBox . map hosts . sHosts $ s]
+  where
+    hosts h = drawHost h (sPorts s)
 
-attributes :: Brick.AttrMap
-attributes = Brick.attrMap Graphics.Vty.defAttr []
+drawHost :: String -> [Int] -> Types.Widget Name
+drawHost host ports = Core.padLeftRight 1 . Border.border . Core.hLimit 16 . Core.vBox $ Core.str host : Border.hBorder : map drawPort ports
+
+drawPort :: Int -> Types.Widget Name
+drawPort = Core.padLeft (Core.Pad 2) . Core.str . show
+
+attributes :: Attr.AttrMap
+attributes = Attr.attrMap Graphics.Vty.defAttr []
