2a429fafea92fb8f111b3a42b94b7ec93d1c2d3d
Raphael Meyer <raphael.akira.meyer@gmail.com>
2024-08-15 19:53:52 +0200

    add cursor to port

--- app/Main.hs
+++ app/Main.hs
@@ -19,7 +19,7 @@ type Port = Int
 
 data ConnectionState = ConnectionState
   { sHosts :: Cursor.Cursor Host,
-    sPorts :: [Port],
+    sPorts :: Cursor.Cursor Port,
     sConnections :: Map.Map Port Host
   }
 
@@ -42,7 +42,7 @@ initialState :: Settings.Settings -> ConnectionState
 initialState settings =
   ConnectionState
     { sHosts = Cursor.makeCursor (Settings.hosts settings),
-      sPorts = Settings.ports settings,
+      sPorts = Cursor.makeCursor (Settings.ports settings),
       sConnections = Map.empty
     }
 
@@ -59,32 +59,40 @@ app =
 drawUI :: ConnectionState -> [Types.Widget Name]
 drawUI s = [Core.hBox . Cursor.mapWith (drawHost (sPorts s)) . sHosts $ s]
 
-drawHost :: [Port] -> Bool -> Host -> Types.Widget Name
-drawHost ports selected host = if selected then Core.withAttr (Attr.attrName "selectedHost") box else box
+drawHost :: Cursor.Cursor Port -> Bool -> Host -> Types.Widget Name
+drawHost ports selected host = Core.padLeftRight 1 . Border.border . Core.hLimit 16 . Core.vBox $ box
   where
-    box = Core.padLeftRight 1 . Border.border . Core.hLimit 16 . Core.vBox $ Core.str host : Border.hBorder : map drawPort ports
+    box = title : Border.hBorder : Cursor.mapWith (drawPort selected) ports
+    title =
+      if selected
+        then Core.withAttr (Attr.attrName "selectedHost") $ Core.str host
+        else Core.str host
 
-drawPort :: Int -> Types.Widget Name
-drawPort = Core.padLeft (Core.Pad 2) . Core.str . show
+drawPort :: Bool -> Bool -> Port -> Types.Widget Name
+drawPort selectedHost selected =
+  if selected && selectedHost
+    then Core.padLeft (Core.Pad 2) . Core.withAttr (Attr.attrName "selectedPort") . Core.str . show
+    else Core.padLeft (Core.Pad 2) . Core.str . show
 
 attributes :: Attr.AttrMap
 attributes =
   Attr.attrMap
     Vty.defAttr
-    [ (Attr.attrName "selectedHost", Util.fg Vty.cyan)
+    [ (Attr.attrName "selectedHost", Util.fg Vty.blue `Vty.withStyle` Vty.bold),
+      (Attr.attrName "selectedPort", Util.fg Vty.green)
     ]
 
 handleEvent :: Types.BrickEvent Name e -> Types.EventM Name ConnectionState ()
 handleEvent (Types.VtyEvent ev) = case ev of
   Vty.EvKey Vty.KEsc [] -> Brick.halt
   Vty.EvKey (Vty.KChar 'q') [] -> Brick.halt
-  Vty.EvKey Vty.KRight [] -> do
-    s <- Types.get
-    let s' = s {sHosts = Cursor.next (sHosts s)}
-    Types.put s'
   Vty.EvKey Vty.KLeft [] -> do
-    s <- Types.get
-    let s' = s {sHosts = Cursor.previous (sHosts s)}
-    Types.put s'
+    Types.modify (\s -> s {sHosts = Cursor.previous (sHosts s)})
+  Vty.EvKey Vty.KRight [] -> do
+    Types.modify (\s -> s {sHosts = Cursor.next (sHosts s)})
+  Vty.EvKey Vty.KUp [] -> do
+    Types.modify (\s -> s {sPorts = Cursor.previous (sPorts s)})
+  Vty.EvKey Vty.KDown [] -> do
+    Types.modify (\s -> s {sPorts = Cursor.next (sPorts s)})
   _ -> pure ()
 handleEvent _ = pure ()
